ac9faa05487a653061a2a1d2b300c03a90cc0bb2
Arvind Mukund <armu30@gmail.com>
2020-09-08 22:38:36 -0400

    clang_format: don't fix the wrong files
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- install_clang_format_hook.sh
+++ install_clang_format_hook.sh
@@ -1,18 +1,26 @@
-#!/ bin / bash
+#!/bin/bash
 
 BASEDIR="$(git rev-parse --show-toplevel)"
 TARGET="$BASEDIR/.git/hooks/pre-commit"
 
-
 cat << EOT > "$TARGET"
-TO_BE_FORMATTED="\$(git diff --name-only --cached)"
+TO_BE_FORMATTED="\$(git diff --name-only --cached | egrep ".+\.java")"
 
-for file in "\${TO_BE_FORMATTED}";
-do
-echo "Clang formatting \$file" clang - format-- style = file \$file -
-                                                        i git add \$file done
+if [ \${#TO_BE_FORMATTED} -eq 0 ]; then
+	echo "No java files detected. Skipping clang-format."
+	exit 0
+fi
 
-                                                                EOT
 
-                                                                        chmod +
-                                                        x "$TARGET"
+echo "Fixing up \$TO_BE_FORMATTED"
+
+for file in "\${TO_BE_FORMATTED}"; do
+	echo "Clang formatting \$file"
+	clang-format --style=file \$file -i
+	git add \$file
+done
+
+EOT
+
+chmod +x "$TARGET"
+
