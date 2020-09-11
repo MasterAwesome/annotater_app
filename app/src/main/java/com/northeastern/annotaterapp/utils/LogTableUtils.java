d6a9f4c22b126fa9be20b9f22f2049898ae68db1
Arvind Mukund <armu30@gmail.com>
2020-09-11 14:14:08 -0400

    More api level 23 changes
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- app/src/main/java/com/northeastern/annotaterapp/utils/LogTableUtils.java
+++ app/src/main/java/com/northeastern/annotaterapp/utils/LogTableUtils.java
@@ -5,9 +5,12 @@ import static com.northeastern.annotaterapp.Constants.DATE_FORMAT;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.util.Log;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 /**
  * Utils class that helps with database operations specifically the Log table.
@@ -24,13 +27,18 @@ public final class LogTableUtils {
     public static void updateTable(Context context, String activity) {
         Log.d(LOG_TAG, "Pushing into the log table!");
         SQLiteDatabase db = context.openOrCreateDatabase("main", Context.MODE_PRIVATE, null);
+        String timestamp;
 
-        LocalDateTime date = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        String text = date.format(formatter);
-
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            timestamp = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
+                                .format(Calendar.getInstance().getTime());
+        } else {
+            LocalDateTime date = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+            timestamp = date.format(formatter);
+        }
         ContentValues cv = new ContentValues();
-        cv.put("TIMESTAMP", text);
+        cv.put("TIMESTAMP", timestamp);
         cv.put("ACTIVITY", activity.trim());
         db.insert("log", null, cv);
 
